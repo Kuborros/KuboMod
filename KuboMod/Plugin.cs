72528b2ac609f500b65a5895333db691f20904b9
Kuborros <kubo@catbat.rocks>
2023-02-11 21:33:44 +0100

    Updated to use NPCLIB

--- KuboMod/Plugin.cs
+++ KuboMod/Plugin.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 
 namespace KuboMod
 {
-    [BepInPlugin("com.kuborro.plugins.fp2.kubomod", "KuboMod", "2.1.1")]
+    [BepInPlugin("com.kuborro.plugins.fp2.kubomod", "KuboMod", "3.0.0")]
+    [BepInDependency("000.kuborro.plugins.fp2.npclib")]
     [BepInProcess("FP2.exe")]
     public class Plugin : BaseUnityPlugin
     {
@@ -23,11 +24,11 @@ namespace KuboMod
                 return;
             }
 
-            //HarmonyFileLog.Enabled = true;
+            kuboObject = moddedBundle.LoadAsset<GameObject>("NPC_Kubo");
+            FP2NPCLib.FP2NPCLib.registerNPC("com.kuborros.kubo","Kubo","Battlesphere Lobby", kuboObject,1,2,8);
+
             var harmony = new Harmony("com.kuborro.plugins.fp2.kubomod");
             harmony.PatchAll(typeof(PatchPommy));
-            harmony.PatchAll(typeof(PatchInstanceNPC));
-            harmony.PatchAll(typeof(PatchNPCList));
             harmony.PatchAll(typeof(PatchTrains));
         }
 
@@ -42,57 +43,8 @@ namespace KuboMod
                     __instance.position = new Vector2(3022, -2456);
                     ___start = new Vector2(3022, -2456);
                     __instance.idleTime = 255;
-                    FPStage.ValidateStageListPos(kuboObject.GetComponent<FPHubNPC>());
                 }
             }   
-        }
-
-        class PatchInstanceNPC
-        {
-            [HarmonyPostfix]
-            [HarmonyPatch(typeof(FPPlayer), "Start", MethodType.Normal)]
-            static void Postfix()
-            {
-                if (FPStage.stageNameString == "Battlesphere Lobby")
-                {
-                    Object[] modKuboPre = moddedBundle.LoadAllAssets();
-                    foreach (var mod in modKuboPre)
-                    {
-                        if (mod.GetType() == typeof(GameObject))
-                        {
-                            kuboObject = (GameObject)Instantiate(mod);
-                            kuboObject.name = "NPC_Kubo_BSphere";                         
-                        }
-                    }
-                }
-            }
-        }
-        class PatchNPCList
-        {
-            [HarmonyPostfix]
-            [HarmonyPatch(typeof(FPSaveManager), nameof(FPSaveManager.LoadFromFile), MethodType.Normal)]
-            static void Postfix(ref string[] ___npcNames)
-            {
-                if (!(___npcNames.Contains("01 02 Kubo")))
-                {
-                    ___npcNames = ___npcNames.AddToArray("01 02 Kubo");
-                }
-
-                if (FPSaveManager.npcFlag.Length < ___npcNames.Length)
-                    FPSaveManager.npcFlag = FPSaveManager.ExpandByteArray(FPSaveManager.npcFlag, ___npcNames.Length);
-                if (FPSaveManager.npcDialogHistory.Length < ___npcNames.Length)
-                    FPSaveManager.npcDialogHistory = FPSaveManager.ExpandNPCDialogHistory(FPSaveManager.npcDialogHistory, ___npcNames.Length);
-
-                int id = FPSaveManager.GetNPCNumber("Kubo");
-                if (id != 0 && FPSaveManager.npcDialogHistory[id].dialog == null)
-                {
-                    FPSaveManager.npcDialogHistory[id].dialog = new bool[8];
-                }
-                else if (id != 0 && FPSaveManager.npcDialogHistory[id].dialog.Length != 8)
-                {
-                    FPSaveManager.npcDialogHistory[id].dialog = new bool[8];
-                }
-            }
         }
 
         class PatchTrains
@@ -109,8 +61,6 @@ namespace KuboMod
                     if (spriteRenderer.sprite.texture.name == "SpriteAtlasTexture-Passengers-256x256-fmt4")
                     {
                         string texPath = Path.Combine(Path.GetFullPath("."), "mod_overrides");
-                        //FileLog.Log("Mods Path: " + texPath + "\\SpriteAtlasTexture_Passengers_256x256_fmt4.png");
-                        //FileLog.Log("Texture file found: " + File.Exists(texPath + "\\SpriteAtlasTexture_Passengers_256x256_fmt4.png").ToString());
                         if (File.Exists(texPath + "\\SpriteAtlasTexture_Passengers_256x256_fmt4.png"))
                         {
                             spriteRenderer.sprite.texture.LoadImage(File.ReadAllBytes(texPath + "\\SpriteAtlasTexture_Passengers_256x256_fmt4.png"));
